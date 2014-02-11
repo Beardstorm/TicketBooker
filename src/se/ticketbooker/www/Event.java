ff7c667ae8afdd8c7d0e84ae3d1599bcef6e5df9
magkur <magnus.kurtto@gmail.com>
2014-02-11 09:39:34 +0100

    Building on event class
    added initiations in constructor

--- src/se/ticketbooker/www/Event.java
+++ src/se/ticketbooker/www/Event.java
@@ -1,9 +1,12 @@
 package se.ticketbooker.www;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.sql.Date;
 import java.sql.Time;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Event extends JPanel{
@@ -15,9 +18,11 @@ public class Event extends JPanel{
 	private String eventDescription;
 	private int numTickets;
 	private JButton book;
+	private JLabel arenaLabel, eventNameLabel, eventDateLabel, eventTimeLabel, ageLimitLabel, descriptionLabel, noOfTicketsLabel;
 	
 	public Event() {
-		this.setLayout(mgr);
+		this.setLayout(new FlowLayout());
+		this.setPreferredSize(new Dimension(200,200));
 		book = new JButton("Buy");
 		
 	}
@@ -30,6 +35,15 @@ public class Event extends JPanel{
 		setAgeLimit(ageLimit);
 		setEventDescription(eventDescription);
 		setNumTickets(numTickets);
+		
+		arenaLabel = new JLabel(getArenaName());
+		eventNameLabel = new JLabel(getEventName());
+		eventDateLabel = new JLabel(getEventDate().toString());
+		eventTimeLabel = new JLabel(getEventTime().toString());
+		ageLimitLabel = new JLabel(getAgeLimit());
+		descriptionLabel = new JLabel(getEventDescription());
+		noOfTicketsLabel = new JLabel(Integer.toString(getNumTickets()));
+		
 	}
 
 	public String getArenaName() {
